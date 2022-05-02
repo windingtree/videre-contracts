f969d9170757055a9fe7cea0e26f72169dc36ff7
mfw78 <mfw78@protonmail.com>
2022-05-02 11:51:55 +0000

    feat: 🎸 timestamp registry

---
+++ contracts/TimestampRegistry.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.13;
+
+import {ITimestampRegistry} from "./interfaces/ITimestampRegistry.sol";
+
+/// @title A simple timestamp registry implementation
+/// @author mfw78 <mfw78@protonmail.com>
+contract TimestampRegistry is ITimestampRegistry {
+  // --- data ---
+  /// @inheritdoc ITimestampRegistry
+  mapping (bytes32 => uint256) public when;
+
+  /// @inheritdoc ITimestampRegistry
+  /// @dev Make sure the hash hasn't already been timestamped.
+  function chop(bytes32 hash) external override {
+    require(when[hash] == 0, "timestamp/already-stamped");
+    when[hash] = block.timestamp;
+  }
+}
---
+++ contracts/interfaces/ITimestampRegistry.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.13;
+
+/// @title A generic timestamp registry interface
+/// @author mfw78 <mfw78@protonmail.com>
+/// @notice Use this timestamp registry to assert when a document existed.
+interface ITimestampRegistry {
+  /// @notice Register the hash at the current `block.timestamp`.
+  /// @param hash of the document being stamped.
+  function chop(bytes32 hash) external;
+
+  /// @notice Get the timestamp of the information's hash.
+  /// @param hash of the document being queried
+  /// @return The timestamp for the document, or 0 if not valid.
+  function when(bytes32 hash) external returns (uint256);
+}
