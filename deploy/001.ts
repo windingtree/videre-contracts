a762e7f74516a52954c045480e80773bc4e4957b
mfw78 <mfw78@protonmail.com>
2022-05-17 14:49:56 +0000

    feat: 🎸 deployment stack for tests

--- deploy/001.ts
+++ deploy/001.ts
@@ -14,20 +14,51 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   console.log(`Bob: ${bob}`)
   console.log(`Carol: ${carol}`)
 
-  // --- Deploy the contract
-  const staysFacilityDeploy = await deploy('StaysFacility', {
+  // --- Deploy the registries
+  const timestampRegistryDeploy = await deploy('TimestampRegistry', {
     from: deployer,
     log: true,
-    autoMine: true, // speed up deployment on local network, no effect on live network.
-    args: ['videre-stays', '1']
+    autoMine: true
   })
 
-  if (staysFacilityDeploy.newlyDeployed) {
-    console.log(
-      `Contract StaysFacility deployed at ${staysFacilityDeploy.address} using ${staysFacilityDeploy.receipt?.gasUsed} gas`
-    )
-  }
+  const serviceProviderRegistryDeploy = await deploy('ServiceProviderRegistry', {
+    from: deployer,
+    log: true,
+    autoMine: true,
+    args: [Number(60 * 60 * 24 * 180).toString()]
+  })
+
+  const lineRegistryDeploy = await deploy('LineRegistry', {
+    from: deployer,
+    log: true,
+    autoMine: true,
+    args: [serviceProviderRegistryDeploy.address, '1']
+  })
+
+  const vatDeploy = await deploy('Vat', {
+    from: deployer,
+    log: true,
+    autoMine: true
+  })
+
+  const mockERC20Deploy = await deploy('MockERC20', {
+    from: deployer,
+    log: true,
+    autoMine: true
+  })
+
+  const gemJoinDeploy = await deploy('GemJoin', {
+    from: deployer,
+    log: true,
+    autoMine: true,
+    args: [
+      'MockERC20GemJoin',
+      '1',
+      vatDeploy.address,
+      mockERC20Deploy.address
+    ]
+  })
 }
 
 export default func
-func.tags = ['StaysFacility']
+func.tags = ['Videre']
